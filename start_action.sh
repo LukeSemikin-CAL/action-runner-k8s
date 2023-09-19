cd6bc2fdc037dd77e9a0f0aa2abe695c58dc2864
Luke Semikin <108271425+LukeSemikin-CAL@users.noreply.github.com>
2023-09-19 11:46:39 +0100

    Version 0.0.1 (#1)
    * Add files via upload
    
    * Update start_action.sh
    
    * Create create_new_release.yml

---
+++ start_action.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+ENVIRONMENT=$ENVIRONMENT
+ORGANISATION=$ORGANISATION
+REPOSITORY=$REPOSITORY
+$GITHUB_PAT=$GITHUB_PAT
+
+REG_TOKEN=$(curl -sX POST -H "Authorization: Bearer ${GITHUB_PAT}" https://api.github.com/repos/${ORGANIZATION}/${REPOSITORY}/actions/runners/registration-token | jq -r .token)
+# REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+
+cd /home/docker/actions-runner
+
+./config.sh --unattended --url https://github.com/${ORGANIZATION}/${REPOSITORY} --name $(hostname)--token ${REG_TOKEN} --labels ${ENVIRONMENT}
+
+cleanup() {
+    echo "Removing runner..."
+    ./config.sh remove --unattended --token ${REG_TOKEN}
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+#Run github action run script and then exit once complete
+./run.sh & wait $! 
